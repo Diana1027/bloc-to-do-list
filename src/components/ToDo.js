ed0507dba86f6d9eff30097c6a6081af0420d6a1
Diana <dspeicher27@gmail.com>
2018-10-12 09:08:43 +1000

    Checkpoint 6: Completed - Modified App.js & Created ToDo.js

---
+++ src/components/ToDo.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class ToDo extends Component {
+    render(){
+        return (
+            <li>A ToDo Will Go Here</li>
+        );
+    }
+}
+export default ToDo;
